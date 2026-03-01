53935ffdfdb6848cf72e6d1afe9aa0424d97af42
theredwillow <theredwillows@gmail.com>
2026-02-28 23:43:21 -0800

    Moved files around to improve 11ty compliance

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/');
-  eleventyConfig.addPassthroughCopy('src/pages/shrines/**/*.png');
-  eleventyConfig.addPassthroughCopy("src/**/*.css");
-  eleventyConfig.addPassthroughCopy("src/**/*.js");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/**/*.{png,js,css}");
+
+  // Get shrine names from the shrines directory
+  eleventyConfig.addGlobalData("shrines", () => {
+    const shrinesDir = path.join(__dirname, 'src', 'shrines');
+    if (!fs.existsSync(shrinesDir)) {
+      return [];
+    }
+    
+    return fs.readdirSync(shrinesDir)
+      .filter(file => fs.statSync(path.join(shrinesDir, file)).isDirectory())
+      .sort();
+  });
 
   return {
     dir: {
